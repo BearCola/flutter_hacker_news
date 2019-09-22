945732b3f185878db6fe7e21f910de4e8328116d
Sangin Kang <sangin84@gmail.com>
2019-09-22 09:57:46 +0900

    update code

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hacker_news/src/blocs/stories_provider.dart';
+import 'package:flutter_hacker_news/src/screens/news_detail.dart';
 import 'screens/news_list.dart';
+import 'blocs/comments_provider.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StoriesProvider(
-      child: MaterialApp(
-        title: 'News!',
-        onGenerateRoute: (RouteSettings settings) {},
+    return CommentsProvider(
+      child: StoriesProvider(
+        child: MaterialApp(
+          title: 'News!',
+          onGenerateRoute: routes,
+        ),
       ),
     );
+  }
+
+  Route routes(RouteSettings settings) {
+    if (settings.name == '/') {
+      return MaterialPageRoute(builder: (context) {
+        return NewsList();
+      });
+    } else {
+      return MaterialPageRoute(builder: (context) {
+        final commentsBloc = CommentsProvider.of(context);
+        final itemId = int.parse(settings.name.replaceFirst('/', ''));
+
+        commentsBloc.fetchItemWithComments(itemId);
+
+        return NewsDetail(
+          itemId: itemId,
+        );
+      });
+    }
   }
 }
